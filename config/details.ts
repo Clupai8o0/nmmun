d4f3a427e18cb873f1c92a8de134d81ea04b896f
Clupai8o0 <clupaio4@gmail.com>
2023-11-19 20:50:38 +0300

    concluded

--- config/details.ts
+++ config/details.ts
@@ -1,5 +1,5 @@
 export const eventDate = "2023-11-16 17:33:00"; // YYYY-MM-DD HH-MM-SS
-export const eventDateInWords = "17th & 18th November, 2023";
+export const eventDateInWords = "NMMUN 2023 has concluded. Join us next year!!";
 
 export const councils = [
 	{
